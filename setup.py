8df7eba908ec3f4f1f5f001b8ddd522622c7ae24
Matthew Nicol <matthew@communityds.com.au>
2019-10-22 12:24:44 +1030

    Prepare to put this on PYPI

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="sqlang",
     version="0.2",
-    packages=find_packages(),
+    author="Matthew Nicol",
+    author_email="matthew.b.nicol@gmail.com",
+    description="Generate SQL programmatically with function calls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matthewnicol/sqlang",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6",
 )
+
+
